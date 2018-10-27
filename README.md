92e4f10c1bde30e72956e942d12e41dd97a81cce
Kevin Baugh <kbaugh@indeed.com>
2018-10-26 19:49:13 -0500

    Added some readme and a css cheatsheet

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup Steps
+
+1. Clone this repository
+1. Run `npm i -g http-server json-server`
+1. Open this repo in terminal and run `http-server .`
+1. To deploy a one off copy/production, run `surge .`
+    * [More info on `surge`](https://surge.sh/)
--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-class CustomContent extends HTMLElement {
-  constructor () {
-    super()
-    console.log(this)
-  }
-}
+/* This will only work in Chrome, so I'm commenting out for now. Feel free to uncomment and see what it does. */
+// class CustomContent extends HTMLElement {
+//   constructor () {
+//     super()
+//     console.log(this.getAttributeNames())
+//     if (this.getAttribute('block')) {
+//       console.log(
+//         `This element is 'display: block because of the block attribute, which is ${!!this.getAttribute('block')}'`
+//       )
+//     }
+//   }
+// }
 
 ;(function () {
-  customElements.define('custom-content', CustomContent)
+  console.log('hold on to your butts')
+  // customElements.define('custom-content', CustomContent)
 })()
