701bc2d55d4938a2ad022e1152de1b638c71f285
Kevin Baugh <loraxx753@gmail.com>
2018-11-12 18:37:30 -0600

    Got everything working in IE... so far (#7)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-import StockTable from './modules/table.js'
+
 import { getAnotherDayGenerator } from './modules/functions.js';
 import { tr, th } from './modules/elements.js'
 
@@ -7,7 +7,6 @@ const getAnotherDay = getAnotherDayGenerator();
 export default (async function () {
   window.days = await fetch('https://api.iextrading.com/1.0/stock/aapl/chart/1y').then(resp => resp.json())
 
-  customElements.define('stock-table', StockTable, {extends: 'table'})
   const days = await fetch('https://api.iextrading.com/1.0/stock/aapl/chart/1y').then(resp => resp.json())
   
   const keys = Object.keys(days[0]).map(item => item.replace(/([A-Z])/g, ' $1').toLowerCase());
